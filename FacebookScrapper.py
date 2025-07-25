30d81d804641ac3be2a449b1b95696f42c738129
inti <inti.resende@gmail.com>
2025-07-25 20:13:23 +1200

    migrated facebook

--- FacebookScrapper.py
+++ FacebookScrapper.py
@@ -9,7 +9,8 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 
-import FileNames
+import FileUtils
+import ScrapperNames
 from EventInfo import EventInfo
 import re
 from datetime import datetime, timedelta
@@ -86,8 +87,16 @@ class FacebookScrapper:
         driver.execute_script(f"window.scrollBy(0, 200);")
         sleep(random.randint(1, 3))
         button = info.find_element(By.XPATH, '//div[@role="button" and text()="See more"]')
-
         button.click()
+
+        sleep(1)
+
+        try:
+            button = info.find_element(By.XPATH, '//div[@role="button" and text()="See more"]')
+            button.click()
+        except:
+            pass
+
         try:
             actions = ActionChains(driver)
             window_size = driver.get_window_size()
@@ -206,12 +215,10 @@ class FacebookScrapper:
         end_date_string = end_date.strftime("%Y-%m-%d")
         end_date_string += "T05%3A00%3A00.000Z"
         category_urls = set()
-        with open(FileNames.FACEBOOK_URLS, mode="r") as f:
-            category_urls = json.loads(f.read())
         events = []
-        out_file = open(FileNames.FACEBOOK_EVENTS, mode="w")
-        out_urls_file = open(FileNames.FACEBOOK_URLS, mode="w")
-        out_urls_file.write("[\n")
+        out_file, urls_file, banned_file = FileUtils.get_files_for_scrapper(ScrapperNames.FACEBOOK)
+        previous_urls = previous_urls.union(set(FileUtils.load_banned(ScrapperNames.FACEBOOK)))
+        urls_file.write("[\n")
         out_file.write("[\n")
         driver.get(
             f"https://www.facebook.com/events/?"
@@ -253,7 +260,7 @@ class FacebookScrapper:
             driver.execute_script("arguments[0].scrollIntoView(true);", cat_button)
             cat_button.click()
             sleep(1)
-            category_urls = category_urls.union(FacebookScrapper.slow_scroll_to_bottom(driver, cat, previous_urls, out_urls_file))
+            category_urls = category_urls.union(FacebookScrapper.slow_scroll_to_bottom(driver, cat, previous_urls, urls_file))
             cat_button.click()
         driver.get(
             f"https://www.facebook.com/events/?"
@@ -264,7 +271,7 @@ class FacebookScrapper:
             f"&end_date={end_date_string}")
         sleep(1)
 
-        category_urls = category_urls.union(FacebookScrapper.slow_scroll_to_bottom_other(driver, previous_urls, out_urls_file, scroll_increment=5000))
+        category_urls = category_urls.union(FacebookScrapper.slow_scroll_to_bottom_other(driver, previous_urls, urls_file, scroll_increment=5000))
 
         driver.get(
             f"https://www.facebook.com/events/?"
@@ -274,8 +281,8 @@ class FacebookScrapper:
             f"&start_date={start_date_string}"
             f"&end_date={end_date_string}")
         sleep(1)
-        category_urls = category_urls.union(FacebookScrapper.slow_scroll_to_bottom_other(driver, previous_urls, out_urls_file, scroll_increment=5000))
-        out_urls_file.write("]\n")
+        category_urls = category_urls.union(FacebookScrapper.slow_scroll_to_bottom_other(driver, previous_urls, urls_file, scroll_increment=5000))
+        urls_file.write("]\n")
         num_events = len(category_urls)
         print(f"fetching: {num_events}")
         count = 1
@@ -290,6 +297,8 @@ class FacebookScrapper:
                 sleep(random.uniform(2, 4))
             except Exception as e:
                 print(e)
+                json.dump(part[0], banned_file, indent=2)
+                banned_file.write(",\n")
                 sleep(random.uniform(2, 4))
             print(f"{count} out of {num_events}")
             count += 1
@@ -298,6 +307,7 @@ class FacebookScrapper:
         driver.close()
         out_file.write("]\n")
         out_file.close()
-        out_urls_file.close()
+        urls_file.close()
+        banned_file.close()
         return events
 # events = list(map(lambda x: x.to_dict(), sorted(FacebookScrapper.fetch_events(), key=lambda k: k.name.strip())))
