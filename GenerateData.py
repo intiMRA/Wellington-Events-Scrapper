2c52b463184b05fca47cdd87041311523fb362d9
inti <inti.albuquerque@trademe.co.nz>
2025-09-14 08:45:26 +1200

    more data migration

--- GenerateData.py
+++ GenerateData.py
@@ -146,7 +146,7 @@ def move_top_n_shortest(num:int, category: str):
                 json.dump(unclassified_data, unclassified_file_write, indent=2)
 
 
-# move_top_n_shortest(1, "Arts & Theatre")
+# move_top_n_shortest(1, "Community & Culture")
 generate_data()
 generate_unclassified_data()
 count_categories()
